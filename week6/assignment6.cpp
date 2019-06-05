decb5f60f34a1d4b0a8e012a8b451be7508b86d7
SethDuffy <seth.duffy.5633@mail.linnbenton.edu>
2019-06-04 19:31:03 -0700

    finished binary search tree

--- week6/assignment6.cpp
+++ week6/assignment6.cpp
@@ -4,30 +4,45 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+//struct to store each positions value and their immediate children
 struct Node {
     int data;
     struct Node *left, *right;
 };
 
+//make a new node that's the size of a node in memory
+//then initailize it's data to be the passed in value
+//and make it have no immediate successors
 struct Node *newNode(int val) { 
    struct Node *tempNode = (struct Node*) malloc(sizeof(struct Node)); 
    tempNode->data = val;
-   tempNode->left = tempNode->right;
    tempNode->right = NULL;
+   tempNode->left = NULL;
 }
 
+//searches the tree recurssively comparing the given value to the ones
 struct Node *searchTree(struct Node* root, int val){
 
    if(root->data == val){
       cout << val << " is in the tree" << endl;
       return root;
    }
-   if(root->data < val){
+   // used to fix tree searching functionality
+   // cout << "searching for " << val << " at " << root->data << endl;
+
+   if(root->data > val){
       return searchTree(root->left, val);
    }
-   return searchTree(root->right, val);
+   else if (root->data < val){
+      return searchTree(root->right, val);
+   }//this doesnt work to correctly catch if a value is not in the list
+   else {
+      cout << "value not in list" << endl;
+      return root; 
+   }
 }
-
+//inserts the value into it's correct respective spot, then adjusts all other values in the list
+//until a NULL node is found to insert furthest down node into
 struct Node* insert(struct Node* node, int val){
 
    if(node == NULL)
@@ -45,22 +60,25 @@ struct Node* insert(struct Node* node, int val){
 void traversal(struct Node *root){
    if(root != NULL){
       traversal(root->left);
-      cout << "Root value: " << root->data << endl;
+      cout << root->data << endl;
       traversal(root->right);
    }
 }
 
 struct Node* deleteNode(struct Node* root, int val){
+
+   //checks for us not having a tree to search
    if(root == NULL) return root;
 
-   //traverse the tree to find the value
+   //walk through the tree similar to the search function until it finds the appropriate value
    if(val < root->data) {
       root->left = deleteNode(root->left, val);
    }
    else if(val > root->data) {
       root->right = deleteNode(root->right, val);
    }
-   else {
+   else {//deletes the value then replaces it with it's successor if it's left or right 
+         //branch respectively has no immediate successor
       if(root->left == NULL) {
          struct Node* temp = root->right;
          delete(root);
@@ -71,14 +89,11 @@ struct Node* deleteNode(struct Node* root, int val){
          delete(root);
          return temp;
       }
+      //moves down the right side of the tree shift their values up successively
+      //not sure if this works for all values
+      root->data = root->right->data;
 
-      struct Node* temp = root->right;
-      while(temp != NULL && temp->left != NULL) {
-         temp = temp->left;
-      }
-      root->data = temp->data;
-
-      root->right = deleteNode(root->right, temp->data);
+      root->right = deleteNode(root->right, root->data);
    }
    return root;
 }
@@ -100,6 +115,18 @@ int main(void){
 
    searchTree(root, 22);
 
+   cout << "deleting 22, 122, and 1" << endl;
    deleteNode(root, 22);
-   searchTree(root, 22);
+   deleteNode(root, 122);
+   deleteNode(root, 1);
+   deleteNode(root, 102);
+
+   insert(root, 7);
+   insert(root, 54);
+   insert(root, 786);
+
+   searchTree(root, 786);
+
+
+   traversal(root);
 }
