711aebce96129b6801c16ec9fe8853c2a8fd6bb8
Seth Duffy <nassabear@gmail.com>
2019-06-10 00:28:54 -0700

    Finished the Graph

--- final/graph.cpp
+++ final/graph.cpp
@@ -58,6 +58,80 @@ void shortestPath(int (&arr)[5][5], int node1, int node2) {
 }
 
 
+void minimumSpanningTree(int (&arr)[5][5], int start) {
+    int graph[5][5];
+    bool done = false;
+    int visited[5] = {0};
+    visited[start] = 1;
+
+    int leastVal = 0, leastValNode = 0;
+    int currNode = start, nextNode = 0;
+    // int temp = 0;
+
+    //copy the array to delete paths without problem
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++) {
+            graph[i][j] = arr[i][j];
+        }
+    }
+    while(!done) {
+        //iterate through all paths a single node has
+        for(int i = 0; i < 5; i++) {
+            //checks for the current nodes path to not be zero and that the node that you're visiting is zero
+            //and that the curernt node that you are at is not equalt to zero
+            if(graph[currNode][i] != 0 && visited[i] == 0 && visited[currNode] != 0){
+                //check for a least value to be found at all
+                if(leastVal == 0) {
+                    leastVal = graph[currNode][i];
+                    leastValNode = currNode;
+                    nextNode = i;
+                } 
+                else if(graph[currNode][i] < leastVal) {
+                    leastVal = graph[currNode][i];
+                    leastValNode = currNode;
+                    nextNode = i;
+                }
+            }
+        }
+        // temp = currNode;
+        // cout << currNode;
+        if(currNode >= 4) {
+            graph[leastValNode][nextNode] = 0;
+            graph[nextNode][leastValNode] = 0;
+
+            visited[nextNode] = 1;
+
+            cout << "We went from " << leastValNode+1 << " to " << nextNode+1 << endl;
+
+            leastValNode = 0;
+            nextNode = 0;
+            leastVal = 0;
+            currNode = 0;
+        }
+        else {
+            currNode++;
+        }
+
+
+        // Old Jank Code
+        // else {
+        //     temp++;
+        //     while(visited[temp] != 1 && temp < 4) {
+        //         temp++;
+        //     }
+        //     currNode = temp;
+        //     temp = 0;
+        // }
+        // // currNode++;
+        done = true;
+        for(int j = 0; j < 5; j++){
+            if(visited[j] == 0){
+                done = false;
+            }
+        }
+    }        
+}
+
 
 
 int main(void) {
@@ -73,7 +147,10 @@ int main(void) {
 
     // shortestPath(graph, 0, 3);
     // shortestPath(graph, 0, 2);
-    shortestPath(graph, 3, 0);
+    // shortestPath(graph, 3, 0);
+    //minimumSpanningTree(graph, 2);
+    //minimumSpanningTree(graph, 0);
+    minimumSpanningTree(graph, 4);
 
     // displayGraph(graph);
 
