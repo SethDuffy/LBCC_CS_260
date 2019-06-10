ccf44717d5d80d7451eed598a38c787721c1a268
Seth Duffy <nassabear@gmail.com>
2019-06-10 01:42:14 -0700

    added more comments to final

--- final/graph.cpp
+++ final/graph.cpp
@@ -6,12 +6,12 @@ using std::cin;
 using std::endl;
 
 void addPath(int (&graph)[5][5],int node1, int node2, int weight) {
-
+    //Adds path to graph for both possible variations of the path
     graph[node1][node2] = weight;
     graph[node2][node1] = weight;
 
 }
-
+//displays graph
 void displayGraph(int (&graph)[5][5]) {
     for(int i = 0; i < 5; i++) {
         for(int j = 0; j < 5; j++) {
@@ -21,7 +21,7 @@ void displayGraph(int (&graph)[5][5]) {
         }
     }
 }
-// from node 1 to node 2
+// from node 1 to node 2 shoutouts to Dijkstra
 void shortestPath(int (&arr)[5][5], int node1, int node2) {
     int graph[5][5];
     int leastVal = 0;
@@ -29,26 +29,33 @@ void shortestPath(int (&arr)[5][5], int node1, int node2) {
     int nextNode = 0;
     int visited[5] = {0};
 
+    cout << "The Shortest Path Using Dijkstra's Algorithm is as Follows" << endl;
+
     for(int i = 0; i < 5; i++){
         for(int j = 0; j < 5; j++) {
             graph[i][j] = arr[i][j];
         }
     }
+    //if not at the end
     while(currNode != node2){
+        //iterate throughout the current node that you're on
         for(int i = 0; i < 5; i++) {
+            //check to see if the path exists and the next node hasn't been visited
             if(graph[currNode][i] != 0 && visited[i] == 0){
+                //check for a least value
                 if(leastVal == 0) {
                     leastVal = graph[currNode][i];
                     nextNode = i;
-                } 
+                } //actual check for smallest weight
                 else if(graph[currNode][i] < leastVal) {
                     leastVal = graph[currNode][i];
                     nextNode = i;
                 }
             }
         }
-
+        //update values
         visited[currNode] = 1;
+        //delete path for future
         graph[currNode][nextNode] = 0;
         graph[nextNode][currNode] = 0;
         cout << "We went from " << currNode+1 << " to " << nextNode+1 << endl;
@@ -67,6 +74,8 @@ void minimumSpanningTree(int (&arr)[5][5], int start) {
     int leastVal = 0, leastValNode = 0;
     int currNode = start, nextNode = 0;
     // int temp = 0;
+
+    cout << "The Minimal Spanning Tree is as Follows" << endl;
 
     //copy the array to delete paths without problem
     for(int i = 0; i < 5; i++){
@@ -147,10 +156,10 @@ int main(void) {
 
     // shortestPath(graph, 0, 3);
     // shortestPath(graph, 0, 2);
-    // shortestPath(graph, 3, 0);
+    shortestPath(graph, 3, 0);
     //minimumSpanningTree(graph, 2);
     //minimumSpanningTree(graph, 0);
-    minimumSpanningTree(graph, 4);
+    minimumSpanningTree(graph, 2);
 
     // displayGraph(graph);
 
